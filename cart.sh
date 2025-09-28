1da955ee269095307b019867a69f40b7685a3150
Asri <asri>
2025-09-28 11:43:42 +0530

    pdate

---
+++ cart.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+userid=$(id -u)
+
+if [ $userid -ne 0 ]; then
+
+Dir=$pwd
+
+echo "error : you have not root privelage acess"
+
+else
+echo :"sucessfully excuted command"
+fi
+
+valid() {
+
+    if [ $1 -ne 0 ]; then
+    echo : "Not Excuted the $2 command"
+
+    else 
+
+        echo : "Sucessfully $2 excuted command"
+    fi
+
+}
+dnf module disable nodejs -y
+valid $? "disable nodejs"
+
+dnf module enable nodejs:20 -y
+valid $? "enable nodejs"
+
+
+dnf install nodejs -y
+valid $? "Install nodejs"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+$Dir
+mkdir /app 
+
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
+cd /app 
+unzip /tmp/cart.zip
+
+$Dir
+cd /app 
+
+npm install 
+valid $? "npm is sucessfully installed"
+
+
+
+systemctl daemon-reload
+valid $? "daemon-reload"
+
+
+systemctl enable cart 
+valid $? "enable cart "
+
+systemctl start cart
+valid $? "startcart"
---
+++ mongo.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+userid=$(id -u)
+
+if [ $userid -ne 0 ]; then
+
+echo "error : you have not root privelage acess"
+
+else
+echo :"sucessfully excuted command"
+fi
+
+valid() {
+
+    if [ $1 -ne 0 ]; then
+    echo : "Not Excuted the $2 command"
+
+    else 
+
+        echo : "Sucessfully $2 excuted command"
+    fi
+
+}
+
+dnf install mongodb-org -y 
+valid $? "insall mongodb"
+
+systemctl enable mongod 
+valid $? "enable mongodb"
+
+
+systemctl start mongod 
+valid $? "start mongodb"
